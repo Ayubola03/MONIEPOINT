4bbf3ceaca8b56aa03d9daa824a67952dccc7192
Ayub <ayubola@gmail.com>
2024-09-20 09:58:05 -0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let x = "";
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: [
+    {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+    {name:"BMW", models:["320", "X3", "X5"]},
+    {name:"Fiat", models:["500", "Panda"]}
+  ]
+}
+
+console.log(myObj.cars[0].models[2])
